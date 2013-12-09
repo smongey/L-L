2fb44c8169e98cc4eebf0a90c0ce64f6b9a0f5ba
Robert Farrelly <hello@robertfarrelly.com>
2013-12-09 22:14:21 +0000

    final update of the day

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,17 @@
 require.config({
-	paths: {
+	paths: {  
+
 		jquery: '../bower_components/jquery/jquery',
 		raphael: '../bower_components/raphael/raphael',
 		localScroll: "../bower_components/jquery.localScroll/jquery.localScroll",
 		scrollTo: "../bower_components/jquery.scrollTo/jquery.scrollTo",
 		swipe: '../bower_components/swipe/swipe',
-		packery: '../bower_components/packery/'
+		bridget: '../bower_components/jquery-bridget/jquery.bridget',
+		packery: '../bower_components/packery/js/packery',
+
+
+	
+
 
 		},
 	shim: {
@@ -17,32 +23,48 @@ require.config({
 			deps: ['jquery'],
 			exports: 'scrollTo'
 		}
+
 	}
 });
 
-require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'modules/app', 'modules/slider', 'modules/nav', 'modules/scrollButton'], 
-	function ($, raphael, localScroll, scrollTo, swipe, app, slider, nav, scrollButton) {
+require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'bridget', 'packery', 'modules/app', 'modules/slider', 'modules/nav', 'modules/scrollButton'], 
+	function ($, raphael, localScroll, scrollTo, swipe, bridget, packery, app, slider, nav, scrollButton) {
 	'use strict';
 
+	$(document).ready(function(){
+		slideTitlePosition();
 
-			$(document).ready(function(){
-					slideTitlePosition();
+		$('#container').packery({
+			  itemSelector: '.item',
+			  stamp: ".stamp",
+			  // columnWidth: ".grid-sizer",
+			  gutter: 50
+		});
 
-			});
+		$('.item').each(function() {
+
+			var thumbHeight = $(this).height();
+			var projectTitle 	   = $('.project-title');
+
+			$(this).find('h3').css({'margin-top': ( ( thumbHeight / 2 ) - $(this).find('h3').height() )  })
+
+		});
+
+	});
 
 
-			$(window).on('resize', function(){
-						slideTitlePosition();
-			});
-					
+	$(window).on('resize', function(){
+		slideTitlePosition();
+	});
+			
 
-			function slideTitlePosition() {
-			var sliderHeight = $('#slider').height() / 2 -30;
-			var slideTitle 	 = $('.slide-title');
-			$(slideTitle).css({'margin-top' : sliderHeight});
-			}
-
+	function slideTitlePosition() {
+	var sliderHeight = $('#slider').height() / 2 -30;
+	var slideTitle 	 = $('.slide-title');
+	$(slideTitle).css({'margin-top' : sliderHeight});
+	}
 
 });
+
 
 	
--- app/scripts/modules/nav.js
+++ app/scripts/modules/nav.js
@@ -6,7 +6,7 @@ define(['jquery'], function() {
 
 		var scrolledFromTop = $(window).scrollTop();
 		var	mainNav 		= $('.main-nav');
-		var	secondaryNav 	= $('.secondary-nav');
+		var	container 		= $('#container');
 		var scrollButton	= $('a.top-button');
 		
 		if ( scrolledFromTop > 10 ) {
@@ -14,12 +14,12 @@ define(['jquery'], function() {
 			if (scrolledFromTop >= $('#slider').height()) {
 				$(mainNav).removeClass('relative').addClass('fixed-top');
 
-				$(secondaryNav).addClass('fixed-top');
+				$(container).addClass('fixed-top');
 				$(scrollButton).attr("href", "#top");
 
 			} else {
 				$(mainNav).addClass('relative').removeClass('fixed-top');
-				$(secondaryNav).removeClass('fixed-top');
+				$(container).removeClass('fixed-top');
 				$(scrollButton).attr("href", "#body-content"); 
 				
 			}
