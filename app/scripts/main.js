e0ed62a6f2790ff4f28ac25b625e39c6e302d4ee
Robert Farrelly <hello@robertfarrelly.com>
2013-12-10 15:51:12 +0000

    yolo sean and pjax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,13 @@
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery',
+        jquery: '../bower_components/jquery/jquery-pjax',
         raphael: '../bower_components/raphael/raphael',
         localScroll: "../bower_components/jquery.localScroll/jquery.localScroll",
         scrollTo: "../bower_components/jquery.scrollTo/jquery.scrollTo",
         swipe: '../bower_components/swipe/swipe',
         bridget: '../bower_components/jquery-bridget/jquery.bridget',
         packery: '../bower_components/packery/js/packery',
+        pjax:     '../bower_components/jquery-pjax/jquery.pjax'
     },
     shim: {
         'localScroll': {
@@ -16,12 +17,29 @@ require.config({
         'scrollTo': {
             deps: ['jquery'],
             exports: 'scrollTo'
+        },
+        'pjax': {
+            deps: ['jquery'],
+            exports: 'pjax'
         }
     }
 });
-require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'bridget', 'packery', 'modules/app', 'modules/slider', 'modules/nav', 'modules/scrollButton', 'modules/imgTitlePos'], 
-	function($, raphael, localScroll, scrollTo, swipe, bridget, packery, app, slider, nav, scrollButton, imgTitlePos) {
+require(['jquery', 'raphael', 'localScroll', 'scrollTo', 'swipe', 'bridget', 'packery', 'pjax', 'modules/app', 'modules/slider', 'modules/nav', 'modules/scrollButton', 'modules/imgTitlePos'], 
+	function($, raphael, localScroll, scrollTo, swipe, bridget, packery, pjax, app, slider, nav, scrollButton, imgTitlePos) {
     'use strict';
 
+
+    var duration = 400;
+    $('a[data-pjax]').pjax('#page', { 
+        fragment: '#page',
+        duration: duration
+    });
+
+
+    $('#page').bind('pjax:start', function() { 
+        $(this).fadeOut(duration); 
+    }).bind('pjax:end', function() { 
+        $(this).fadeIn(duration); 
+    });
 
 });
